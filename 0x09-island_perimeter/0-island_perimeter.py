c868ce3ba1b3b69f7957bbd9a52f79610671a37d
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-12-06 01:21:16 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to calculate the perimeter of an island in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in grid.
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                # Check up
+                if r == 0 or grid[r-1][c] == 0:
+                    perimeter += 1
+                # Check down
+                if r == rows-1 or grid[r+1][c] == 0:
+                    perimeter += 1
+                # Check left
+                if c == 0 or grid[r][c-1] == 0:
+                    perimeter += 1
+                # Check right
+                if c == cols-1 or grid[r][c+1] == 0:
+                    perimeter += 1
+
+    return perimeter
