5ea50a9137b704a87e6fb3e3f2a398a4bc2ef57a
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-12-11 04:11:29 +0100

    modify code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,58 +1,39 @@
 #!/usr/bin/python3
-"""
-Prime Game
-"""
+"""Prime Game"""
+
 
 def isWinner(x, nums):
     """
-    Determine the winner of the Prime Game.
-
-    :param x: Number of rounds
-    :param nums: Array of integers representing the maximum number for each round
-    :return: Name of the player that won the most rounds, or None if it's a tie
+    Determines the winner of the prime game
+    Args:
+        x (int): number of rounds
+        nums (list): array of n
+    Returns:
+        str: name of the player that won the most rounds
+        None: if winner cannot be determined
     """
-    def is_prime(n):
-        """Check if a number is prime."""
-        if n <= 1:
-            return False
-        if n <= 3:
-            return True
-        if n % 2 == 0 or n % 3 == 0:
-            return False
-        i = 5
-        while i * i <= n:
-            if n % i == 0 or n % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-
-    def get_primes(n):
-        """Get all prime numbers up to n."""
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def can_win(n):
-        """Determine if the first player can win given n."""
-        primes = get_primes(n)
-        if not primes:
-            return False
-        return len(primes) % 2 == 1
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if can_win(n):
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if not nums or x < 1:
         return None
+
+    n = max(nums)
+    # Create sieve of Eratosthenes array
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+
+    # Mark non-prime numbers in sieve
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+
+    # Count prime numbers up to each n in nums
+    maria_wins = 0
+    for n in nums:
+        prime_count = sum(1 for i in range(2, n + 1) if sieve[i])
+        # If number of primes is odd, Maria wins
+        if prime_count % 2 == 1:
+            maria_wins += 1
+
+    if maria_wins * 2 == x:
+        return None
+    return 'Maria' if maria_wins * 2 > x else 'Ben'
